8ab74b0fb062f586f6d88bca2b12d63d39f3dbc1
Mônica A. C. Valadão <45488379+monicavaladao@users.noreply.github.com>
2021-08-28 10:02:13 -0300

    Update construa_matriz_caso_geral.m

--- FuncoesOctave/construa_matriz_caso_geral.m
+++ FuncoesOctave/construa_matriz_caso_geral.m
@@ -1,31 +1,31 @@
 function [A] = construa_matriz_caso_geral(regra_ii,regra_ij,cond_ij,valor_cond,m,n)
-% CONSTRUA_MATRIz: FunÁ„o que retorna a matriz A de ordem mxn
-% correspondente a definiÁ„o fornecida pelas entradas da funÁ„o.
+% CONSTRUA_MATRIz: Fun√ß√£o que retorna a matriz A de ordem mxn
+% correspondente a defini√ß√£o fornecida pelas entradas da fun√ß√£o.
 % Entrada:
-%   regra_ii: Regra associada a condiÁ„o cond_ij = cond_valor, sendo que i,j 
-%   representam, %   respectivamente, linha e coluna de A. Essa regra deve ser 
+%   regra_ii: Regra associada a condi√ß√£o cond_ij = valor_cond, sendo que i,j 
+%   representam, respectivamente, linha e coluna de A. Essa regra deve ser 
 %   inserida na forma de @(i,j)().
-%   regra_ij: Regra associada a condiÁ„o cond_ij = valor_cond, sendo que i,j 
+%   regra_ij: Regra associada a condi√ß√£o cond_ij = valor_cond, sendo que i,j 
 %   representam, respectivamente, linha e coluna de A. Essa regra deve ser 
 %   inserida na forma de @(i,j)().
 %   cond_ij: Condicao estabeleciada aos indices i,j da matriz. Essa regra deve 
 %   ser inserida na forma de @(i,j)().
 %   valor_cond: Valor associado a cond_ij.
-%   m: N˙mero de linhas de A.
-%   n: N˙mero de colunas de A.
-% SaÌda:
+%   m: N√∫mero de linhas de A.
+%   n: N√∫mero de colunas de A.
+% Sa√≠da:
 %   A: Matriz A de ordem mxn
 % Exemplo: 
-%   Duas condiÁıes para os Ìndices
+%   Duas condi√ß√µes para os √≠ndices
 %   [A] = construa_matriz_caso_geral(@(i,j)(2*i),@(i,j)(j+2),@(i,j)(i+j),4,4,7)
-%   Apenas uma condiÁ„o para os Ìndices
+%   Apenas uma condi√ß√£o para os √≠ndices
 %   [A] = construa_matriz_caso_geral(@(i,j)(2*i),@(i,j)(2*i),@(i,j)(i+j),4,4,7)
 %==========================================================================
-% Projeto Proae: ElaboraÁ„o de Material Did·tico que Empregue o uso de 
-%   Software como Suporte para o Aprendizado de ¡lgebra Linear.
+% Projeto Proae: Elabora√ß√£o de Material Did√°tico que Empregue o uso de 
+%   Software como Suporte para o Aprendizado de √Ålgebra Linear.
 % Membros do Projeto:
-%   MÙnica Aparecida Cruvinel Valad„o (Coordenadora)
-%   Douglas Frederico Guimar„es  Santiago (Vice-Coordenador)
+%   M√¥nica Aparecida Cruvinel Valad√£o (Coordenadora)
+%   Douglas Frederico Guimar√£es  Santiago (Vice-Coordenador)
 %   Anderson Luiz Pedrosa Porto (Colaborador)
 %   Flaviano Luiz Benfica (Bolsista)
 %==========================================================================
