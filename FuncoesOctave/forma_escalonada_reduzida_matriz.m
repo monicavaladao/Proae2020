85a5870b5bfb2d106203ce59bc2a27bd9b549059
Mônica A. C. Valadão <45488379+monicavaladao@users.noreply.github.com>
2021-08-28 10:57:09 -0300

    Update forma_escalonada_reduzida_matriz.m

--- FuncoesOctave/forma_escalonada_reduzida_matriz.m
+++ FuncoesOctave/forma_escalonada_reduzida_matriz.m
@@ -1,32 +1,32 @@
 function [E, pA, nA] = forma_escalonada_reduzida_matriz(A)
-% FORMA_ESCALONADA_REDUZIDA: FunÁ„o que retorna a forma escalonda reduzida
+% FORMA_ESCALONADA_REDUZIDA: Fun√ß√£o que retorna a forma escalonda reduzida
 % da matriz A.
 % Entrada:
 %   A: Matriz de ordem mxn.
-% SaÌda:
-%   E: Matriz na forma escalonada reduzida equivalente a A, no caso de n„o
-%      ocorrer erro ao usar a funÁ„o rref().
-%   E: [] no caso de ocorrer erro ao usar a funÁ„o rref().
+% Sa√≠da:
+%   E: Matriz na forma escalonada reduzida equivalente a A, no caso de n√£o
+%      ocorrer erro ao usar a fun√ß√£o rref().
+%   E: [] no caso de ocorrer erro ao usar a fun√ß√£o rref().
 %   pA: Posto de A.
 %   nA: Nulidade de A.
 % Exemplo:
 %   [E,pA,nA] = forma_escalonada_reduzida_matriz([1,2,-3,0;2,4,-2,2;3,6,-4,3])
 %==========================================================================
-%Importante: O uso dessa funÁ„o para encontrar a forma escalonada reduzida em um
-% car·ter mais acadÍmico. Com o uso da funÁ„o rref()podem ocorrer erros numÈrios.
-% Dependendo da toler‚ncia tol estabelecida para rref(), tem-se resultados 
+%Importante: O uso dessa fun√ß√£o para encontrar a forma escalonada reduzida em um
+% car√°ter mais acad√™mico. Com o uso da fun√ß√£o rref()podem ocorrer erros num√©rios.
+% Dependendo da toler√¢ncia tol estabelecida para rref(), tem-se resultados 
 % diferentes. 
 %==========================================================================
-% Projeto Proae: ElaboraÁ„o de Material Did·tico que Empregue o uso de 
-%   Software como Suporte para o Aprendizado de ¡lgebra Linear.
+% Projeto Proae: Elabora√ß√£o de Material Did√°tico que Empregue o uso de 
+%   Software como Suporte para o Aprendizado de √Ålgebra Linear.
 % Membros do Projeto:
-%   MÙnica Aparecida Cruvinel Valad„o (Coordenadora)
-%   Douglas Frederico Guimar„es  Santiago (Vice-Coordenador)
+%   M√¥nica Aparecida Cruvinel Valad√£o (Coordenadora)
+%   Douglas Frederico Guimar√£es  Santiago (Vice-Coordenador)
 %   Anderson Luiz Pedrosa Porto (Colaborador)
 %   Flaviano Luiz Benfica (Bolsista)
 %==========================================================================
 
-% Determina o n˙mero de linhas e de colunas de A
+% Determina o n√∫mero de linhas e de colunas de A
 [m,n] = size(A);
 
 % Determina o posto de A
@@ -39,7 +39,7 @@ nA = n - pA;
 E = rref(A);
 % Verifica se pode ter ocorrido erro com o uso de rref
 m_absE = rank(E);
-% Verifica se o n˙mero de linhas de absE È diferente de pA
+% Verifica se o n√∫mero de linhas de n√£o nulas de E √© diferente de pA
 if m_absE ~= pA
     fprintf('Possivel erro associado a tolerancia de rref()!\n');
     E = [];
