c5288b82fae149bce0db244d0b1e0443c65b1321
Mônica A. C. Valadão <45488379+monicavaladao@users.noreply.github.com>
2021-08-28 11:07:29 -0300

    Update inversa_matriz.m

--- FuncoesOctave/inversa_matriz.m
+++ FuncoesOctave/inversa_matriz.m
@@ -1,20 +1,20 @@
 function [detA,invA] = inversa_matriz(A)
-% INVERSA_MATRIZ: FunÁ„o que calcula a inversa da matriz A, caso exista.
+% INVERSA_MATRIZ: Fun√ß√£o que calcula a inversa da matriz A, caso exista.
 % Entrada:
 %   %   A: Matriz A de ordem nxn.
-% SaÌda:
+% Sa√≠da:
 %   detA: Determinante da matriz A.
 %   invA:
 %       Matriz inversa de A de ordem nxn, caso exista.
-%       [] se A n„o possui inversa.
+%       [] se A n√£o possui inversa.
 % Exemplo:
 %   [detA,invA] = inversa_matriz([2 3 -2;1 4 8;7 5 3])
 %==========================================================================
-% Projeto Proae: ElaboraÁ„o de Material Did·tico que Empregue o uso de 
-%   Software como Suporte para o Aprendizado de ¡lgebra Linear.
+% Projeto Proae: Elabora√ß√£o de Material Did√°tico que Empregue o uso de 
+%   Software como Suporte para o Aprendizado de √Ålgebra Linear.
 % Membros do Projeto:
-%   MÙnica Aparecida Cruvinel Valad„o (Coordenadora)
-%   Douglas Frederico Guimar„es  Santiago (Vice-Coordenador)
+%   M√¥nica Aparecida Cruvinel Valad√£o (Coordenadora)
+%   Douglas Frederico Guimar√£es  Santiago (Vice-Coordenador)
 %   Anderson Luiz Pedrosa Porto (Colaborador)
 %   Flaviano Luiz Benfica (Bolsista)
 %==========================================================================
@@ -22,17 +22,17 @@ function [detA,invA] = inversa_matriz(A)
 % Calcula a ordem da matriz A
 [m,n] = size(A);
 
-% Verifica a condiÁ„o para calcular o determinante
+% Verifica a condi√ß√£o para calcular o determinante
 if m == n
     % Calcula o determinate da matriz A
     detA = det(A);  
 
-    % Devido a erros numÈricos n„o È aconselh·vel usar o determinante para verificar
+    % Devido a erros num√©ricos n√£o √© aconselh√°vel usar o determinante para verificar
     % se uma matriz quadrada A possui inversa. Para essa finalizade deve-se verificar
-    % o condicionamento da matriz em quest„o. Nesse sentido, usa-se aqui a funÁ„o
-    % inv(), a qual retorna um warning no caso de A n„o ser bem condicionada.
+    % o condicionamento da matriz em quest√£o. Nesse sentido, usa-se aqui a fun√ß√£o
+    % inv(), a qual retorna um warning no caso de A n√£o ser bem condicionada.
     
-    % Limpa, caso exista, o ˙ltimo lastwarn retornado
+    % Limpa, caso exista, o √∫ltimo lastwarn retornado
     lastwarn('');
     warning('');
     % Tenta calcular a inversa da matriz A
@@ -45,7 +45,7 @@ if m == n
     if (isempty(msg))
       invA = invA;
     else
-      fprintf('A matriz n„o possui inversa!!!\n');
+      fprintf('A matriz n√£o possui inversa!!!\n');
       invA = [];
     end      
 
@@ -53,12 +53,12 @@ if m == n
 %        % Calcula a inversa da matriz A
 %        invA = inv(A);
 %    else
-%        fprintf('A matriz n„o possui inversa!!!');
+%        fprintf('A matriz n√£o possui inversa!!!');
 %        invA = [];
 %    end
 
 else
-    fprintf('A matriz n„o È quadrada!!!');
+    fprintf('A matriz n√£o √© quadrada!!!\n');
     detA = [];
     invA = [];
 end
