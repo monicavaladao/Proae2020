a5190c72a8b1e19e4a7abb3017bb7034eae21735
Mônica A. C. Valadão <45488379+monicavaladao@users.noreply.github.com>
2021-08-28 16:11:52 -0300

    Update subtracao_matrizes.m

--- FuncoesOctave/subtracao_matrizes.m
+++ FuncoesOctave/subtracao_matrizes.m
@@ -1,18 +1,18 @@
 function [C] = subtracao_matrizes(A,B)
-% SOMA_MATRIZES: FunÁ„o que retorna a matriz C = A - B.
+% SUBTRACAO_MATRIZES: Fun√ß√£o que retorna a matriz C = A - B.
 % Entrada:
 % A: Matriz de ordem mxn.
 % B: Matriz de ordem mxn.
-% SaÌda:
+% Sa√≠da:
 % C: Matriz C = A+B de ordem mxn.
 % Exemplo:
 %   [C] = subtracao_matrizes([1,2,3;4,5,6],[3,2,1;9,8,7])
 %==========================================================================
-% Projeto Proae: ElaboraÁ„o de Material Did·tico que Empregue o uso de 
-%   Software como Suporte para o Aprendizado de ¡lgebra Linear.
+% Projeto Proae: Elabora√ß√£o de Material Did√°tico que Empregue o uso de 
+%   Software como Suporte para o Aprendizado de √Ålgebra Linear.
 % Membros do Projeto:
-%   MÙnica Aparecida Cruvinel Valad„o (Coordenadora)
-%   Douglas Frederico Guimar„es  Santiago (Vice-Coordenador)
+%   M√¥nica Aparecida Cruvinel Valad√£o (Coordenadora)
+%   Douglas Frederico Guimar√£es  Santiago (Vice-Coordenador)
 %   Anderson Luiz Pedrosa Porto (Colaborador)
 %   Flaviano Luiz Benfica (Bolsista)
 %==========================================================================
@@ -23,12 +23,12 @@ function [C] = subtracao_matrizes(A,B)
 % Calcula a ordem da matriz B
 [m_B,n_B] = size(B);
 
-% Verifica se a soma est· definida
+% Verifica se a soma est√° definida
 if m_A == m_B && n_A == n_B
     % Calcula a matriz C
     C = A - B;
 else
-    fprintf('SubtraÁ„o de matrizes n„o est· definida. Verifique as ordens das matrizes!!!\n')
+    fprintf('Subtra√ß√£o de matrizes n√£o est√° definida. Verifique as ordens das matrizes!!!\n')
     C = [];
 end
 
