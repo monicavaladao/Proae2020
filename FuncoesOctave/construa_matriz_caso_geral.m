d2ce27287328766c6a99aaee834773bdc43ad120
Mônica A. C. Valadão <45488379+monicavaladao@users.noreply.github.com>
2021-08-28 10:11:03 -0300

    Update construa_matriz_caso_geral.m

--- FuncoesOctave/construa_matriz_caso_geral.m
+++ FuncoesOctave/construa_matriz_caso_geral.m
@@ -1,14 +1,14 @@
 function [A] = construa_matriz_caso_geral(regra_ii,regra_ij,cond_ij,valor_cond,m,n)
-% CONSTRUA_MATRIz: Função que retorna a matriz A de ordem mxn
+% CONSTRUA_MATRIZ: Função que retorna a matriz A de ordem mxn
 % correspondente a definição fornecida pelas entradas da função.
 % Entrada:
 %   regra_ii: Regra associada a condição cond_ij = valor_cond, sendo que i,j 
 %   representam, respectivamente, linha e coluna de A. Essa regra deve ser 
 %   inserida na forma de @(i,j)().
-%   regra_ij: Regra associada a condição cond_ij = valor_cond, sendo que i,j 
+%   regra_ij: Regra associada a condição cond_ij ~= valor_cond, sendo que i,j 
 %   representam, respectivamente, linha e coluna de A. Essa regra deve ser 
 %   inserida na forma de @(i,j)().
-%   cond_ij: Condicao estabeleciada aos indices i,j da matriz. Essa regra deve 
+%   cond_ij: Condição estabeleciada aos indices i,j da matriz. Essa regra deve 
 %   ser inserida na forma de @(i,j)().
 %   valor_cond: Valor associado a cond_ij.
 %   m: Número de linhas de A.
