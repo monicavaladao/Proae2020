3ebb065318d6cf734536cd08a74a836ca7505f49
Mônica A. C. Valadão <45488379+monicavaladao@users.noreply.github.com>
2021-08-28 16:19:15 -0300

    Update verifica_autovalor_matriz.m

--- FuncoesOctave/verifica_autovalor_matriz.m
+++ FuncoesOctave/verifica_autovalor_matriz.m
@@ -1,20 +1,20 @@
 function [p] = verifica_autovalor_matriz(A,lambda)
-% VERIFICA_AUTOVALOR_MATRIZ: FunÁ„o que retorna o valor p(lambda) onde p(.) È o
-% polinÙmio caracterÌstico de A
+% VERIFICA_AUTOVALOR_MATRIZ: Fun√ß√£o que retorna o valor p(lambda) onde p(.) √© o
+% polin√¥mio caracter√≠stico de A
 % Entrada:
 %   A: Matriz de ordem nxn.
 %   lambda: Escalar qualquer.
-% SaÌda:
+% Sa√≠da:
 %   p: Valor dado por p(lambda).
 % Exemplo:
 %   [p] = verifica_autovalor_matriz([-3,1,-1;-7,5,-1;-6,6,-2],-2)
 %
 %==========================================================================
-% Projeto Proae: ElaboraÁ„o de Material Did·tico que Empregue o uso de 
-%   Software como Suporte para o Aprendizado de ¡lgebra Linear.
+% Projeto Proae: Elabora√ß√£o de Material Did√°tico que Empregue o uso de 
+%   Software como Suporte para o Aprendizado de √Ålgebra Linear.
 % Membros do Projeto:
-%   MÙnica Aparecida Cruvinel Valad„o (Coordenadora)
-%   Douglas Frederico Guimar„es  Santiago (Vice-Coordenador)
+%   M√¥nica Aparecida Cruvinel Valad√£o (Coordenadora)
+%   Douglas Frederico Guimar√£es  Santiago (Vice-Coordenador)
 %   Anderson Luiz Pedrosa Porto (Colaborador)
 %   Flaviano Luiz Benfica (Bolsista)
 %==========================================================================
@@ -23,7 +23,7 @@ function [p] = verifica_autovalor_matriz(A,lambda)
 
 % Verifica a ordem da matriz
 if m == n
-    % Encontra os coeficientes do polinÙmio caracterÌstico
+    % Encontra os coeficientes do polin√¥mio caracter√≠stico
     c = poly(A);
     
     % Retorna os coeficientes considerando o determinante de (A - lambda.*I)
@@ -33,7 +33,7 @@ if m == n
     % Identifica a quantidade de coeficientes
     nc = length(c);
  
-    % Verifica se lambda È soluÁ„o do polinÙmio caracterÌstico
+    % Verifica se lambda √© solu√ß√£o do polin√¥mio caracter√≠stico
     xlambda = lambda*ones(1,(nc-1));
     e = [1:(nc-1)];
     [ex,~] = sort(e,'descend');
@@ -41,7 +41,7 @@ if m == n
     xlambda_ex = xlambda_ex(:);
     p = (c(1:(nc - 1)))'*xlambda_ex + c(nc);
 else
-    fprintf('A matriz n„o È quadrada!!!');
+    fprintf('A matriz n√£o √© quadrada!!!\n');
     p = [];
 end
 
