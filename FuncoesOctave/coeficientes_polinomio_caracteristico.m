dd529c6fcdb5f8e7308bb3aa3f14c231eef75dab
Mônica A. C. Valadão <45488379+monicavaladao@users.noreply.github.com>
2021-08-28 09:42:49 -0300

    Update coeficientes_polinomio_caracteristico.m

--- FuncoesOctave/coeficientes_polinomio_caracteristico.m
+++ FuncoesOctave/coeficientes_polinomio_caracteristico.m
@@ -1,34 +1,34 @@
 function [c] = coeficientes_polinomio_caracteristico(A)
-%COEFICIENTES_POLIN‘MIO_CARACTERISTICO: FunÁ„o que retorna os coeficientes
-%do polinÙmio caracterÌstico associado a matriz A.
+%COEFICIENTES_POLINOMIO_CARACTERISTICO: Fun√ß√£o que retorna os coeficientes
+%do polin√¥mio caracter√≠stico associado a matriz A.
 % Entrada:
 %   A: Matriz A de ordem nxn
-% SaÌda:
-%   c: Vetor de ordem (n+1)x1 que contÈm os coeficientes do polinÙmio
-%       caracterÌstico. Os elementos em c est„o associados ordenadamente
-%       aos termos de maior grau para o termo de menor grau do polinÙmio.
+% Sa√≠da:
+%   c: Vetor de ordem (n+1)x1 que cont√©m os coeficientes do polin√¥mio
+%       caracter√≠stico. Os elementos em c est√£o associados ordenadamente
+%       aos termos de maior grau para o termo de menor grau do polin√¥mio.
 % Exemplo:
 %   [c] = coeficientes_polinomio_caracteristico([-3,1,-1;-2,5,-1;-6,7,-8])
 %==========================================================================
-% Projeto Proae: ElaboraÁ„o de Material Did·tico que Empregue o uso de 
-%   Software como Suporte para o Aprendizado de ¡lgebra Linear.
+% Projeto Proae: Elabora√ß√£o de Material Did√°tico que Empregue o uso de 
+%   Software como Suporte para o Aprendizado de √Ålgebra Linear.
 % Membros do Projeto:
-%   MÙnica Aparecida Cruvinel Valad„o (Coordenadora)
-%   Douglas Frederico Guimar„es  Santiago (Vice-Coordenador)
+%   M√¥nica Aparecida Cruvinel Valad√£o (Coordenadora)
+%   Douglas Frederico Guimar√£es  Santiago (Vice-Coordenador)
 %   Anderson Luiz Pedrosa Porto (Colaborador)
 %   Flaviano Luiz Benfica (Bolsista)
 %==========================================================================
 % Calcula a ordem da matriz A
 [m,n] = size(A);
 
-% Verifica se A È uma matriz quadrada
+% Verifica se A √© uma matriz quadrada
 if m == n
-    % Encontra os coeficientes do polinÙmio caracterÌstico
+    % Encontra os coeficientes do polin√¥mio caracter√≠stico
     c = poly(A);
     % Retorna os coeficientes considerando o determinante de (A - autoval.*I)
     c = -c;
 else
-    fprintf('A matriz n„o È quadrada!!!');
+    fprintf('A matriz n√£o √© quadrada!!!');
     c = [];
 end
 
